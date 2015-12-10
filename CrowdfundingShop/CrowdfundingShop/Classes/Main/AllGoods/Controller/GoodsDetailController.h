386fbf4f5a9a4e7a9a98e7e7d10d155c5875c7da
Evans <368757898@qq.com>
2015-12-11 00:56:34 +0800

    增加详情页面

---
+++ CrowdfundingShop/CrowdfundingShop/Classes/Main/AllGoods/Controller/GoodsDetailController.h
@@ -0,0 +1,41 @@
+//
+//  GoodsDetailController.h
+//  CrowdfundingShop
+//
+//  Created by 吴金林 on 15/12/11.
+//  Copyright © 2015年 吴金林. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ARLabel.h"
+@interface GoodsDetailController : UITableViewController
+/**商品图片*/
+@property (weak, nonatomic) IBOutlet UIImageView *goodsImageView;
+/**商品名字*/
+@property (weak, nonatomic) IBOutlet ARLabel *goodsNameLabel;
+/**商品描述*/
+@property (weak, nonatomic) IBOutlet ARLabel *goodsDescLabel;
+/**进度条*/
+@property (weak, nonatomic) IBOutlet UIProgressView *goodsProgressView;
+/**已参与*/
+@property (weak, nonatomic) IBOutlet ARLabel *goodsLabel1;
+/**总需人次*/
+@property (weak, nonatomic) IBOutlet ARLabel *goodsLabel2;
+/**剩余人次*/
+@property (weak, nonatomic) IBOutlet ARLabel *goodsLabel3;
+/**商品晒单次数*/
+@property (weak, nonatomic) IBOutlet ARLabel *goodsNumLabel;
+/**获得者头像*/
+@property (weak, nonatomic) IBOutlet UIImageView *peopleImageView;
+/**获得者电话号码*/
+@property (weak, nonatomic) IBOutlet ARLabel *peoplePhoneLabel;
+/**获得者所在城市*/
+@property (weak, nonatomic) IBOutlet ARLabel *peopleCityLabel;
+/**商品揭晓时间*/
+@property (weak, nonatomic) IBOutlet ARLabel *timeLabel1;
+/**商品购买时间*/
+@property (weak, nonatomic) IBOutlet ARLabel *timeLabel2;
+/**用户云购码*/
+@property (weak, nonatomic) IBOutlet ARLabel *numberLabel;
+
+@end
---
+++ CrowdfundingShop/CrowdfundingShop/Classes/Main/AllGoods/Controller/GoodsDetailController.m
@@ -0,0 +1,98 @@
+//
+//  GoodsDetailController.m
+//  CrowdfundingShop
+//
+//  Created by 吴金林 on 15/12/11.
+//  Copyright © 2015年 吴金林. All rights reserved.
+//
+
+#import "GoodsDetailController.h"
+
+@interface GoodsDetailController ()
+
+@end
+
+@implementation GoodsDetailController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.goodsImageView.layer.borderWidth=0.5;
+    self.goodsImageView.layer.borderColor=[[UIColor colorWithRed:222.0/255.0 green:222.0/255.0 blue:222.0/255.0 alpha:1.0]CGColor];
+    /**圆角*/
+    [[self.goodsImageView layer]setCornerRadius:2.0];
+    //更改进度条高度
+    self.goodsProgressView.transform=CGAffineTransformMakeScale(1.0f, 2.0f);
+    //设置进度值并动画显示
+    [self.goodsProgressView setProgress:0.7 animated:YES];
+    /**头像圆角*/
+    self.peopleImageView.layer.cornerRadius=30.0;
+    self.peopleImageView.layer.masksToBounds=YES;
+}
+
+
+
+/*
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 0;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    
+    // Configure the cell...
+    
+    return cell;
+}
+*/
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
